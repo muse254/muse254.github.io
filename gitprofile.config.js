0208bd871228b74c45d9f99d56062e1fee9842ca
Osoro Bironga <fanosoro@gmail.com>
2023-05-06 13:49:22 +0300

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,7 +2,7 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'muse254', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
@@ -11,56 +11,50 @@ const config = {
     },
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
-    facebook: '',
-    instagram: '',
-    dribbble: '',
-    behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    stackoverflow: '', // format: userid/username
-    skype: '',
-    telegram: '',
-    website: 'https://arifszn.com',
+    linkedin: 'https://www.linkedin.com/in/osoro-bironga-6646b0185/',
+    //  twitter: 'arif_szn',
+    //  mastodon: 'arifszn@mastodon.social',
+    //  facebook: '',
+    //  instagram: '',
+    //  dribbble: '',
+    //  behance: '',
+    //  medium: 'arifszn',
+    //  dev: 'arifszn',
+    stackoverflow: '10020745/Osoro', // format: userid/username
+    //  skype: '',
+    //  telegram: '',
+    website: 'https://osoro.dev',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'fanosoro@gmail.com',
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1Z3AaA6aR2KrQy9PUyEFtNXAeblBfY2L-/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
+    'Go',
+    'Rust',
+    'Haskell',
     'Node.js',
-    'Nest.js',
     'MySQL',
     'PostgreSQL',
     'Git',
     'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
+      company: 'Doctaroo',
+      position: 'Microservices Developer',
+      from: 'June 2020',
+      to: 'August 2020',
+      companyLink: 'https://doctaroo.com/',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Rabbit Care',
+      position: 'Software Engineer',
+      from: 'February 2022',
+      to: 'April 2023',
+      companyLink: 'https://rabbitcare.com/en',
     },
   ],
   /* certifications: [
@@ -73,52 +67,52 @@ const config = {
   ], */
   education: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'Moi University',
+      degree: 'B.Sc in Computer Science',
+      from: '2019',
+      to: '2022',
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'Nairobi School',
+      degree: 'High School Degree',
+      from: '2013',
+      to: '2017',
     },
   ],
 
   // To hide the `My Projects` section, keep it empty.
   externalProjects: [
     {
+      title: 'Speak Programming Language',
+      description:
+        'Speak is an experimental programming language that aims to have configurable keywords. This is in an attempt to allow for the use of multiple written languages as preferred by the programmer.',
+      imageUrl: 'https://github.com/muse254/speak/raw/main/assets/logo.png',
+      link: 'https://github.com/muse254/speak',
+    },
+  /*  {
       title: 'Project Name',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
       imageUrl: 'https://via.placeholder.com/250x250',
       link: 'https://example.com',
-    },
-    {
-      title: 'Project Name',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
-    },
+    }, */
   ],
   // Display blog posts from your medium or dev account. (Optional)
-  blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many posts to display. Max is 10.
-  },
-  googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-  },
+  //   blog: {
+  //     source: 'dev', // medium | dev
+  //     username: 'arifszn', // to hide blog section, keep it empty
+  //     limit: 2, // How many posts to display. Max is 10.
+  //   },
+  //   googleAnalytics: {
+  //     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+  //   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
     id: '',
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'winter',
+    defaultTheme: 'retro',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
@@ -178,11 +172,11 @@ const config = {
   },
 
   // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> and ❤️`,
+//   footer: `Made with <a 
+//       class="text-primary" href="https://github.com/arifszn/gitprofile"
+//       target="_blank"
+//       rel="noreferrer"
+//     >GitProfile</a> and ❤️`,
 };
 
 export default config;
